115c75b8521a7557896b83c6bd95bc2a6113fb86
mohamed maher <mohamedmaher.mm330@gmail.com>
2022-08-07 14:48:20 +0200

    add navbar and upload files

---
+++ main.js
@@ -0,0 +1,41 @@
+AOS.init();
+
+// const swiper = new Swiper(".swiper", {
+//   // Optional parameters
+//   direction: "vertical",
+//   loop: true,
+//   speed: 2000,
+//   spaceBetween: 100,
+//   allowSlideNext: true,
+//   allowTouchMove: true,
+//   // If we need pagination
+//   pagination: {
+//     el: ".swiper-pagination",
+//     clickable: true,
+//     currentClass: "swiper-pagination-clickable",
+//     dynamicBullets: true,
+
+//     // bulletActiveClass: "swiper-pagination-bullet-active",
+//   },
+//   effect: "creative",
+//   creativeEffect: {
+//     next: {
+//       // will set `translateX(100%)` on next slides
+//       translate: ["100%", 0, 0],
+//     },
+//   },
+//   autoplay: {
+//     delay: 2000,
+//   },
+
+// Navigation arrows
+//   navigation: {
+//     nextEl: ".swiper-button-next",
+//     prevEl: ".swiper-button-prev",
+//   },
+
+// And if we need scrollbar
+//   scrollbar: {
+//     el: ".swiper-scrollbar",
+//   },
+// });
